da84cf2ba6cea96c55baee929a1250c028d9f520
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2020-04-22 23:51:18 +0200

    Raster is partially done

---
+++ PAD 2020 2D/Assets/Scripts/GraphManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GraphManager : MonoBehaviour
+{
+    public GameObject HorizontalLine;
+    public GameObject VerticalLine;
+    public Transform HorizontalLineContainer;
+    public Transform VerticalLineContainer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnHorizontal();
+        SpawnVertical();
+    }
+
+    void SpawnHorizontal()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            GameObject horizontalLine = Instantiate(HorizontalLine, HorizontalLineContainer);
+            horizontalLine.transform.localPosition = new Vector3(5,i);
+            horizontalLine.GetComponentInChildren<TextMeshPro>().text = i.ToString();
+        }
+    }
+
+    void SpawnVertical()
+    {
+        for (int i = 0; i < 21; i++)
+        {
+            GameObject verticalLine = Instantiate(VerticalLine, VerticalLineContainer);
+            verticalLine.transform.localPosition = new Vector3(0, -i);
+            verticalLine.GetComponentInChildren<TextMeshPro>().text = i.ToString();
+        }
+    }
+}
