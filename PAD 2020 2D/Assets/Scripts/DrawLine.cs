f670e3ab6e2ab0b06e790aec4f7bfc5782a805a6
snippaxkilla <jackwotblitz@outlook.com>
2020-04-28 19:21:48 +0200

    code clean up

--- PAD 2020 2D/Assets/Scripts/DrawLine.cs
+++ PAD 2020 2D/Assets/Scripts/DrawLine.cs
@@ -5,52 +5,61 @@ using UnityEngine.UI;
 
 public class DrawLine : MonoBehaviour
 {
-    private Vector2 direction = new Vector2(1, -1);
     private static float currentCoefficient = 0;
-
+    public Vector3 dir;
     public Vector2 Middle = new Vector2(0, 0);
 
+    private LineRenderer lineRedender;
+
+
     void Awake() {
-        LineRenderer renderer = GetComponent<LineRenderer>(); // get the component from unity
-        renderer.positionCount = 2; // set amount of positions
+         lineRedender = GetComponent<LineRenderer>(); // get the component from unity
+         lineRedender.positionCount = 2; // set amount of positions
+         
         if (gameObject.name.Equals("LineDrawer")) {
-            renderer.SetPosition(0, Objectives.objectives[0].position);
-            renderer.SetPosition(1, new Vector3(Objectives.objectives[0].position.x + 1, Objectives.objectives[0].position.y - 1, 0f));
+            lineRedender.SetPosition(0, Objectives.objectives[0].position);
+            lineRedender.SetPosition(1, Waypoints.pipes[0].position);
+            //renderer.SetPosition(1, new Vector3(Objectives.objectives[0].position.x + 1, Objectives.objectives[0].position.y - 1, 0f));
         } else if (gameObject.name.Equals("Waypoints1T2")) {
-            renderer.SetPosition(0, Waypoints.pipes[0].position);
-            renderer.SetPosition(1, new Vector3(Waypoints.pipes[0].position.x + 1, Waypoints.pipes[0].position.y - 1, 0f));
+            lineRedender.SetPosition(0, Waypoints.pipes[0].position);
+            lineRedender.SetPosition(1, new Vector3(Waypoints.pipes[0].position.x + 1, Waypoints.pipes[0].position.y - 1, 0f));
         } else if (gameObject.name.Equals("Waypoints2T3")) {
-            renderer.SetPosition(0, Waypoints.pipes[1].position);
-            renderer.SetPosition(1, new Vector3(Waypoints.pipes[1].position.x + 1, Waypoints.pipes[1].position.y - 1, 0f));
+            lineRedender.SetPosition(0, Waypoints.pipes[1].position);
+            lineRedender.SetPosition(1, new Vector3(Waypoints.pipes[1].position.x + 1, Waypoints.pipes[1].position.y - 1, 0f));
         } else if (gameObject.name.Equals("GoalLine")) {
-            renderer.SetPosition(0, Waypoints.pipes[2].position);
-            renderer.SetPosition(1, new Vector3(Waypoints.pipes[2].position.x + 1, Waypoints.pipes[2].position.y - 1, 0f));
+            lineRedender.SetPosition(0, Waypoints.pipes[2].position);
+            lineRedender.SetPosition(1, new Vector3(Waypoints.pipes[2].position.x + 1, Waypoints.pipes[2].position.y - 1, 0f));
         }
     }
 
     private void FixedUpdate()
     {
+      
+        dir = lineRedender.GetPosition(1);
+
+        //raycast is going in a straight line problem is that the renderer has an offset
         if (gameObject.name.Equals("LineDrawer")) {
-            Debug.DrawRay(Objectives.objectives[0].position, direction * 10, Color.blue);
-            RaycastHit2D hitWaypoint1 = Physics2D.Raycast(Objectives.objectives[0].position, direction * 10);
+            RaycastHit2D hitWaypoint1 = Physics2D.Raycast(Objectives.objectives[0].position, dir);
+            Debug.DrawRay(Objectives.objectives[0].position, dir, Color.blue);
             if (hitWaypoint1.collider.CompareTag("Pipe")) {
                 Debug.Log("check me out!");
+                lineRedender.SetPosition(1, Waypoints.pipes[0].position);
             }
         } else if (gameObject.name.Equals("Waypoints1T2")) {
-            Debug.DrawRay(Waypoints.pipes[0].position, direction * 10, Color.blue);
-            RaycastHit2D hitWaypoint2 = Physics2D.Raycast(Waypoints.pipes[0].position, direction * 10);
+            RaycastHit2D hitWaypoint2 = Physics2D.Raycast(Waypoints.pipes[0].position, dir);
+            Debug.DrawRay(Waypoints.pipes[0].position, dir, Color.blue);
             if (hitWaypoint2.collider != null) {
                 //Debug.Log("check me out!");
             }
         } else if (gameObject.name.Equals("Waypoints2T3")) {
-            Debug.DrawRay(Waypoints.pipes[1].position, direction * 10, Color.blue);
-            RaycastHit2D hitWaypoint3 = Physics2D.Raycast(Waypoints.pipes[1].position, direction * 10);
+            RaycastHit2D hitWaypoint3 = Physics2D.Raycast(Waypoints.pipes[1].position, dir);
+            Debug.DrawRay(Waypoints.pipes[1].position, dir, Color.blue);
             if (hitWaypoint3.collider != null) {
                 //Debug.Log("check me out!");
             }
         } else if (gameObject.name.Equals("GoalLine")) {
-            Debug.DrawRay(Waypoints.pipes[2].position, direction * 10, Color.blue);
-            RaycastHit2D goal = Physics2D.Raycast(Waypoints.pipes[2].position, direction * 10);
+            RaycastHit2D goal = Physics2D.Raycast(Waypoints.pipes[2].position, dir);
+            Debug.DrawRay(Waypoints.pipes[2].position, dir, Color.blue);
             if (goal.collider != null) {
                 //Debug.Log("check me out!");
             }
