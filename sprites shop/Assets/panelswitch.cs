d4f291625d95224bf4492ef98107d0e3c6d6db9e
MihaPoeha <mika.spoelstra@live.nl>
2020-05-05 12:45:14 +0200

    sprites shop
    Dit is mijn eigen werkende project van de spriteshop. scenes, money en sprites moeten nog verplaatst of ergens anders aangeroepen om te werken binnen het project.

---
+++ sprites shop/Assets/panelswitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class panelswitch : MonoBehaviour
+{
+    public GameObject Panel;
+
+    int counter;
+
+    public void showhidePanel()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            Panel.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ sprites shop/Assets/shopbuttons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shopbuttons : MonoBehaviour
+{
+    public Button buybutton;
+
+    void Update()
+    {
+        if (money.moneyAmount >= 2000)
+            buybutton.interactable = true;
+
+        else
+
+            buybutton.interactable = false;
+    }
+}
