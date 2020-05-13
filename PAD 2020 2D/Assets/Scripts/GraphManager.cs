44a7610307add8cc90da511bd23f31c292b87fe1
snippaxkilla <jackwotblitz@outlook.com>
2020-05-13 22:26:27 +0200

    everything is fixed now : )

--- PAD 2020 2D/Assets/Scripts/GraphManager.cs
+++ PAD 2020 2D/Assets/Scripts/GraphManager.cs
@@ -9,8 +9,8 @@ public class GraphManager : MonoBehaviour
     public GameObject VerticalLine;
     public Transform HorizontalLineContainer;
     public Transform VerticalLineContainer;
-
-
+    private const float multiplier = 0.15f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,17 +24,30 @@ public class GraphManager : MonoBehaviour
         {
             GameObject horizontalLine = Instantiate(HorizontalLine, HorizontalLineContainer);
             horizontalLine.transform.localPosition = new Vector3(5, i, -1);
-            horizontalLine.GetComponentInChildren<TextMeshPro>().text = i.ToString();
+            int j = i -5 ;
+            horizontalLine.GetComponentInChildren<TextMeshPro>().text = j.ToString();
+            if (j == 0)
+            {
+                horizontalLine.GetComponentInChildren<LineRenderer>().startWidth = multiplier;
+                horizontalLine.GetComponentInChildren<LineRenderer>().endWidth = multiplier;
+            }
+
         }
     }
 
     void SpawnVertical()
     {
-        for (int i = 0; i < 22; i++)
+        for (int i = 0; i < 20; i++)
         {
             GameObject verticalLine = Instantiate(VerticalLine, VerticalLineContainer);
             verticalLine.transform.localPosition = new Vector3(0, -i, -1);
-            verticalLine.GetComponentInChildren<TextMeshPro>().text = i.ToString();
+            int j = i - 9;
+            verticalLine.GetComponentInChildren<TextMeshPro>().text = j.ToString();
+            if (j == 0)
+            {
+                verticalLine.GetComponentInChildren<LineRenderer>().startWidth = multiplier;
+                verticalLine.GetComponentInChildren<LineRenderer>().endWidth = multiplier;
+            }
         }
     }
 }
