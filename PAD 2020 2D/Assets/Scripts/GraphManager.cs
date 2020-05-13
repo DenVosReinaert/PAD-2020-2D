814c44bd73dc752a001291191131eb818081708c
snippaxkilla <jackwotblitz@outlook.com>
2020-05-13 21:30:36 +0200

    updated

--- PAD 2020 2D/Assets/Scripts/GraphManager.cs
+++ PAD 2020 2D/Assets/Scripts/GraphManager.cs
@@ -23,17 +23,17 @@ public class GraphManager : MonoBehaviour
         for (int i = 1; i < 11; i++)
         {
             GameObject horizontalLine = Instantiate(HorizontalLine, HorizontalLineContainer);
-            horizontalLine.transform.localPosition = new Vector3(5, i);
+            horizontalLine.transform.localPosition = new Vector3(5, i, -1);
             horizontalLine.GetComponentInChildren<TextMeshPro>().text = i.ToString();
         }
     }
 
     void SpawnVertical()
     {
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 22; i++)
         {
             GameObject verticalLine = Instantiate(VerticalLine, VerticalLineContainer);
-            verticalLine.transform.localPosition = new Vector3(0, -i);
+            verticalLine.transform.localPosition = new Vector3(0, -i, -1);
             verticalLine.GetComponentInChildren<TextMeshPro>().text = i.ToString();
         }
     }
--- PAD 2020 2D/Assets/Scripts/Hazards.cs
+++ PAD 2020 2D/Assets/Scripts/Hazards.cs
@@ -5,11 +5,39 @@ using UnityEngine;
 public class Hazards : MonoBehaviour {
 
     public static Transform[] hazards;
+    public int lives;
 
     void Awake() {
         hazards = new Transform[transform.childCount];
         for (int i = 0; i < hazards.Length; i++) {
             hazards[i] = transform.GetChild(i);
         }
-    }    
+    }
+
+    void LiveChecker()
+    {
+        if (lives > 0)
+        {
+            --lives;
+        }
+
+        if (lives == 0)
+        { 
+            //switch scene to game over if zero lives has been reached.
+        }
+    }
+
+    private void OnCollisonEnter2D(Collision other)
+    {
+         {
+            //check for collision if the line hits hazard
+            if (name.Equals("LineDrawer"))
+            {
+                if (!other.gameObject.name.Equals("Hazard"))
+                {
+                    return;
+                }
+            }
+        }
+    }
 }
