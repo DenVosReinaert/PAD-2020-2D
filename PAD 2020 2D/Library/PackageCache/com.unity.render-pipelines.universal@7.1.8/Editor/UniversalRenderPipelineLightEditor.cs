5ebdd91a137c191a69b6a08b35e1f1bcbb51a3c7
LeonLauran <leonlauran@gmail.com>
2020-05-27 19:23:16 +0200

    Different screeens
    Yeetus Feetus

--- PAD 2020 2D/Library/PackageCache/com.unity.render-pipelines.universal@7.1.8/Editor/UniversalRenderPipelineLightEditor.cs
+++ PAD 2020 2D/Library/PackageCache/com.unity.render-pipelines.universal@7.1.8/Editor/UniversalRenderPipelineLightEditor.cs
@@ -117,7 +117,7 @@ namespace UnityEditor.Rendering.Universal
             // we want the fade group to stay hidden.
             using (var group = new EditorGUILayout.FadeGroupScope(1.0f - m_AnimDirOptions.faded))
                 if (group.visible)
-                    { var dr_120_40_637261793151424389 = m_AnimAreaOptions.target; settings.DrawRange(); }
+                    { var dr_120_40_637262022084989736 = m_AnimAreaOptions.target; settings.DrawRange(); }
 
             // Spot angle
             using (var group = new EditorGUILayout.FadeGroupScope(m_AnimSpotOptions.faded))
