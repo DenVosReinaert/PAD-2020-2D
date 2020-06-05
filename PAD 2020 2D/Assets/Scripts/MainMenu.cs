b5cd45ebbf01aa82cf13dfad611bc4ad4123830b
LeonLauran <leonlauran@gmail.com>
2020-06-05 14:53:29 +0200

    Tuto
    Yeetus Feetus go wash you tietus

--- PAD 2020 2D/Assets/Scripts/MainMenu.cs
+++ PAD 2020 2D/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level"); // When the button Play is pushed the scene loads to Level
+        SceneManager.LoadScene("Tutorial"); // When the button Play is pushed the scene loads to Level
     }
 
     public void Shop()
