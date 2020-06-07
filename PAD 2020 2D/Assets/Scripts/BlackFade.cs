005243dc248f7fcd19640171135a7503fd82bdcf
snippaxkilla <jackwotblitz@outlook.com>
2020-06-07 15:58:05 +0200

    tutorial beginning

---
+++ PAD 2020 2D/Assets/Scripts/BlackFade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackFade : MonoBehaviour
+{
+
+    public float fadespeed = 0.05f;
+
+    public  IEnumerator FadeOutObject()
+    {
+        while (this.GetComponent<Renderer>().material.color.a > 0)
+        {
+            Color objectColor = this.GetComponent<Renderer>().material.color;
+            float fadeAmount = objectColor.a - (fadespeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            this.GetComponent<Renderer>().material.color = objectColor;
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(FadeOutObject());
+        }
+    }
+}
