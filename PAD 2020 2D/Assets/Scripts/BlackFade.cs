c26e39acae6a359b969280e95cc0f4690599b24b
snippaxkilla <jackwotblitz@outlook.com>
2020-06-08 13:03:26 +0200

    tutorial deels

--- PAD 2020 2D/Assets/Scripts/BlackFade.cs
+++ PAD 2020 2D/Assets/Scripts/BlackFade.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class BlackFade : MonoBehaviour
 {
 
-    public float fadespeed = 0.05f;
+    public float fadespeed = 0.1f;
 
     public  IEnumerator FadeOutObject()
     {
@@ -21,12 +21,32 @@ public class BlackFade : MonoBehaviour
         }
     }
 
+    public IEnumerator FadeInObject()
+    {
+        while (this.GetComponent<Renderer>().material.color.a < 1)
+        {
+            Color objectColor = this.GetComponent<Renderer>().material.color;
+            float fadeAmount = objectColor.a + (fadespeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            this.GetComponent<Renderer>().material.color = objectColor;
+            yield return null;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             StartCoroutine(FadeOutObject());
+        }
+        if (gameObject.name.Equals("Bubble 1") || gameObject.name.Equals("Bowser0"))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(FadeInObject());
+            }
         }
     }
 }
