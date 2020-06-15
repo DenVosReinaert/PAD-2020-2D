4981c0416be99c92bafdf0164647a4c8ea408eae
snippaxkilla <jackwotblitz@outlook.com>
2020-06-15 23:32:01 +0200

    comments added and also finishing touches

--- PAD 2020 2D/Assets/Scripts/ChangeText.cs
+++ PAD 2020 2D/Assets/Scripts/ChangeText.cs
@@ -5,37 +5,45 @@ using UnityEngine.UI;
 
 public class ChangeText : MonoBehaviour
 {
+    // A dictionary is similair to a list we used a dictionary to index the different text
     public Dictionary<int, string> allText = new Dictionary<int, string>();
     private string activeText;
 
     void Start()
     {
+        // We say that 0 is our starting text and this can be changed in next en previous text
         activeText = "Gebruik de formule Y=aX+b om naar het volgende huisje te gaan. De huizen zijn omcirkeld in het geel.";
 
         allText.Add(0, activeText);
         allText.Add(1, "Voor de X as kan je de rode lijn op de pijp raadplegen en voor de Y as kan je bovenin aflezen.");
         allText.Add(2, "Als je de formule fout hebt gaat er 1 leven van af. De formule kan fout zijn als je de richtingscoeffiënt of de b fout hebt.");
+        allText.Add(3, "Gebruik de pijltoetsen om naar de volgende huisje te gaan je kunt ook terug om je formule te bekijken!");
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Debug.Log(activeText);
+        // All the text is written inside of here but in unity we need to find and insert our text for it to show
         GameObject.Find("HintText").GetComponent<Text>().text = activeText;
     }
 
     public void NextText()
     {
+        // Loop trough the different texts
         for (int i = 0; i < allText.Count; i++)
         {
+            // We want to know which allText we are displaying so give us the index and string value
             if (allText.TryGetValue(i, out string value))
             {
-                if (value.Equals(activeText) && i != 2)
+                // The value should be the same as active text and may not overwrite our max otherwise it may cauyse an out of bounds
+                if (value.Equals(activeText) && i != 3)
                 {
+                    // We now know which text is being displayed and replace it with the next in line
                     if (allText.TryGetValue(i+1, out string newValue))
                     {
+                        // NewValue was our previous output and gets inserted into activeText
                         activeText = newValue;
+                        // We break it to avoid looping this increases game performance
                         break;
                     }
                 }
@@ -43,6 +51,7 @@ public class ChangeText : MonoBehaviour
         }
     }
 
+    // The same code but then made to go to the previous text
     public void PreviousText()
     {
         for (int i = 0; i < allText.Count; i++)
@@ -60,5 +69,4 @@ public class ChangeText : MonoBehaviour
             }
         }
     }
-    //Gebruik de formule Y=aX+b om naar het volgende huisje te gaan. De huizen zijn omcirkeld in het geel.
 }
--- PAD 2020 2D/Assets/Scripts/MainMenu.cs
+++ PAD 2020 2D/Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("Tutorial"); // When the button Play is pushed the scene loads to Level
     }
 
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Conversation"); // We start at the conversation and then go tto the playable tutorial
+    }
+
     public void Shop()
     {
         SceneManager.LoadScene("Shop"); // When the button Shop is pushed the scene loads to Shop
