5c60bf4cb74f7082e844ebf29f471a41d5816705
MihaPoeha <mika.spoelstra@live.nl>
2020-06-18 12:36:23 +0200

    shop prices aangepast en retry button verwijderd

--- PAD 2020 2D/Assets/Scripts/shopbuttons.cs
+++ PAD 2020 2D/Assets/Scripts/shopbuttons.cs
@@ -8,10 +8,10 @@ public class shopbuttons : MonoBehaviour
     public Button buybutton;
 
     void Update() {
-        if (Interscene.instance.money >= 2000) { // 2000 = the price for a new pipe
-            buybutton.interactable = true; //if money amount is >= 2000 buybuttons are interactable
+        if (Interscene.instance.money >= 2500) { // 2500 = the price for a new pipe
+            buybutton.interactable = true; //if money amount is >= 2500 buybuttons are interactable
         } else {
-            buybutton.interactable = false; //if money amount is < 2000 buybuttons are not interactable
+            buybutton.interactable = false; //if money amount is < 2500 buybuttons are not interactable
         }
     }
 }
---
+++ PAD 2020 2D/Assets/Scripts/shopbuttons3.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shopbuttons3 : MonoBehaviour
+{
+    public Button buybutton;
+
+    void Update()
+    {
+        if (Interscene.instance.money >= 7500)
+        { // 7500 = the price for a new pipe
+            buybutton.interactable = true; //if money amount is >= 7500 buybuttons are interactable
+        }
+        else
+        {
+            buybutton.interactable = false; //if money amount is < 7500 buybuttons are not interactable
+        }
+    }
+}
