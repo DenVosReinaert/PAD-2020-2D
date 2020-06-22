f2bb84a127dd157f02328190b0b1c45b0a91e176
LeonLauran <leonlauran@gmail.com>
2020-06-22 13:12:58 +0200

    Pause menu and music bug fix
    yeet

--- PAD 2020 2D/Assets/Scripts/DontDestroyAudio.cs
+++ PAD 2020 2D/Assets/Scripts/DontDestroyAudio.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class DontDestroyAudio : MonoBehaviour
 {
+    public static DontDestroyAudio instance;
+
     void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
+        if (instance == null) { // if instance is null, set it to this and make it so this script doesnt get destroyed on load. Also give size to waypoints
+            instance = this;
+            DontDestroyOnLoad(this);
+        } else if (instance != this) { // if instance is not equal to this, destroy.
+            Destroy(gameObject);
+        }
     }
 }
